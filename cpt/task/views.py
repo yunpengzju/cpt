5b4fccff87e991f3a469dbb65cc42bf82956d541
Kahaani <czhtd@126.com>
2013-06-25 19:24:06 +0800

    task basic framework

--- cpt/task/views.py
+++ cpt/task/views.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 from django.shortcuts import render_to_response
 from django.http import HttpResponseRedirect
 from django.template import RequestContext
@@ -13,7 +14,7 @@ TEMPLATE_CONTEXT_PROCESSORS = global_settings.TEMPLATE_CONTEXT_PROCESSORS
 
 @permission_required('task.is_member', login_url="/login/")
 def task_list(request):
-    result = Task.objects.order_by("-date")
+    result = Task.objects.order_by("-date") # 按时间倒序
     return render_to_response('task/list.html', locals(), context_instance=RequestContext(request))
 
 @permission_required('task.is_member', login_url="/login/")
@@ -42,16 +43,17 @@ def task_one(request, task_id):
 def task_edit(request, task_id):
     try:
         inst = Task.objects.get(id=task_id)
-
         visitor = Contact.objects.get(nickname = request.user.username)
+
+        # 发布者在状态0下有权限
         if visitor.number != inst.post_id or inst.state != 0:
-            return HttpResponseRedirect('/task/'+str(task_id)+'/')
+            return HttpResponseRedirect('/task/'+str(task_id)+'/operation/')
         
         if request.method == 'POST':
             form = TaskForm(request.POST, instance = inst)
             if form.is_valid():
                 form.save()
-                return HttpResponseRedirect('/task/')
+                return HttpResponseRedirect('/task/'+str(task_id)+'/')
         else:
             form = TaskForm(instance = inst);
         return render_to_response('task/edit.html', locals(), context_instance=RequestContext(request))
@@ -60,10 +62,87 @@ def task_edit(request, task_id):
     except Task.DoesNotExist:
         return HttpResponseRedirect('/task/')
 
+@permission_required('task.is_member', login_url="/login/")
+def task_operation(request, task_id):
+    xietiao = Xietiao.objects.filter(task = task_id)
+#    for item in xietiao:
+#        try:
+#            item
+#        except Contact.DoesNotExist:
+
+    
+    jiangjie = Jiangjie.objects.filter(task = task_id)
+    print jiangjie[0].task
+    
+    return render_to_response('task/operation.html', locals(), context_instance=RequestContext(request))
 
 @permission_required('task.is_manage', login_url="/task/")
-def task_manage(request, task_id):
+def task_close(request, task_id):
+    try:
+        task = Task.objects.get(id=task_id)
+        # 管理者在状态0下有权限
+        if task.state == 0:
+            task.state = 1;
+            task.save()
+        return HttpResponseRedirect('/task/'+str(task_id)+'/operation/')
+    except Task.DoesNotExist:
+        return HttpResponseRedirect('/task/')
+
+@permission_required('task.is_manage', login_url="/task/")
+def task_del_jj(request, task_id, member_id):
     return
+
+@permission_required('task.is_manage', login_url="/task/")
+def task_del_xt(request, task_id, member_id):
+    return
+
+@permission_required('task.is_member', login_url="/login/")
+def task_jj(request, task_id):
+    try:
+        task = Task.objects.get(id=task_id)
+        visitor = Contact.objects.get(nickname = request.user.username)
+        # 队员在状态0下有权限
+        if task.state == 0:
+            Jiangjie.objects.create(
+                task = task_id,
+                member = visitor.number
+            )
+        return HttpResponseRedirect('/task/'+str(task_id)+'/operation/')
+    except Contact.DoesNotExist:
+        return HttpResponseRedirect('/task/')
+    except Task.DoesNotExist:
+        return HttpResponseRedirect('/task/')
+
+@permission_required('task.is_member', login_url="/login/")
+def task_xt(request, task_id):
+    try:
+        task = Task.objects.get(id=task_id)
+        visitor = Contact.objects.get(nickname = request.user.username)
+        # 队员在状态0下有权限
+        if task.state == 0:
+            Xietiao.objects.create(
+                task = task_id,
+                member = visitor.number
+            )
+        return HttpResponseRedirect('/task/'+str(task_id)+'/operation/')
+    except Contact.DoesNotExist:
+        return HttpResponseRedirect('/task/')
+    except Task.DoesNotExist:
+        return HttpResponseRedirect('/task/')
+    return
+
+@permission_required('task.is_member', login_url="/login/")
+def task_contacted(request, task_id):
+    return
+
+@permission_required('task.is_member', login_url="/login/")
+def task_cancel(request, task_id):
+    return
+
+@permission_required('task.is_member', login_url="/login/")
+def task_success(request, task_id):
+    return
+
 
 def db_new_task(formdata, post_nickname):
     post_info = Contact.objects.get(nickname = post_nickname)
